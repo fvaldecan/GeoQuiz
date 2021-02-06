a62cdc8d49ed1d112a5216aa616cd70fb693c775
Franco Dominic Valdecanas <valdecan@sonoma.edu>
2021-02-05 20:58:57 -0800

    Ch2 Challenges: TextView Listener, Previous Button, Button to ImageButton

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -4,19 +4,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
-    private lateinit var nextButton: Button
+    private lateinit var prevButton: ImageButton
+    private lateinit var nextButton: ImageButton
     private lateinit var questionTextView: TextView
     private val questionBank = listOf(
-            Question(R.string.question_australia, false),
+            Question(R.string.question_australia, true),
             Question(R.string.question_oceans, true),
-            Question(R.string.question_mideast, true),
-            Question(R.string.question_africa, true),
+            Question(R.string.question_mideast, false),
+            Question(R.string.question_africa, false),
             Question(R.string.question_americas, true),
             Question(R.string.question_asia, true)
     )
@@ -29,6 +31,7 @@ class MainActivity : AppCompatActivity() {
 //        References content view
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
+        prevButton = findViewById(R.id.prev_button)
         nextButton = findViewById(R.id.next_button)
         questionTextView = findViewById(R.id.question_text_view)
 
@@ -40,6 +43,14 @@ class MainActivity : AppCompatActivity() {
             checkAnswer(false)
         }
         nextButton.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+        prevButton.setOnClickListener {
+            currentIndex = (currentIndex - 1 + questionBank.size) % questionBank.size
+            updateQuestion()
+        }
+        questionTextView.setOnClickListener { view: View ->
             currentIndex = (currentIndex + 1) % questionBank.size
             updateQuestion()
         }
