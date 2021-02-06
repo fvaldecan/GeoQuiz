45c12ffbf215fff2883fe818dcacbb4aa317bec2
Franco Dominic Valdecanas <valdecan@sonoma.edu>
2021-02-05 20:11:21 -0800

    ch2

---
+++ app/src/main/java/com/example/geoquiz/Question.kt
@@ -0,0 +1,7 @@
+package com.example.geoquiz
+
+// NOTE: Recommended (1. Readability 2. Prevents runtime crash)
+import androidx.annotation.StringRes
+
+data class Question(@StringRes val textResId: Int, val answer: Boolean)
+
