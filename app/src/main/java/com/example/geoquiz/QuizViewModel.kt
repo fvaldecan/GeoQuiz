5420531ccdb9c8679542df2714623ee300481a71
Franco Dominic Valdecanas <valdecan@sonoma.edu>
2021-02-06 13:44:25 -0800

    GoeQuiz Ch4

---
+++ app/src/main/java/com/example/geoquiz/QuizViewModel.kt
@@ -0,0 +1,22 @@
+package com.example.geoquiz
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+
+private const val TAG = "QuizViewModel"
+class QuizViewModel : ViewModel(){
+    private val questionBank = listOf(
+        Question(R.string.question_australia, false),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, true),
+        Question(R.string.question_africa, true),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true)
+    )
+    var currentIndex = 0
+    val cuurrentQuestionAnswer: Boolean get() = questionBank[currentIndex].answer
+    val currentQuestionText: Int get() = questionBank[currentIndex].textResId
+    fun moveToNext(){
+        currentIndex = (currentIndex + 1) % questionBank.size
+    }
+}
